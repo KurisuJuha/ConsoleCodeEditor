e93f9e09cea8a538a8e03df0d8c19e910e70cd59
KYapp <kukiyomenaisapp@gmail.com>
2022-07-13 23:53:11 +0900

    plugins.cceにパスを書くことで外部プラグインを読み込めるようになった

--- ConsoleCodeEditor/Plugin/PluginLoader.cs
+++ ConsoleCodeEditor/Plugin/PluginLoader.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,11 +21,23 @@ namespace ConsoleCodeEditor.Plugin
 
         public static void LoadPlugin(string name)
         {
+            List<string> PluginPath = new List<string>(0);
+            using (StreamReader sr = new StreamReader("Plugins.cce",Encoding.UTF8))
+            {
+                string PluginData = sr.ReadToEnd();
+                PluginPath.AddRange(PluginData.Split('\n'));
+            }
+
+            PluginPath.AddRange(Directory.GetFiles(name));
+
             //EXEの場所にあるDLLファイルをすべて読み込む
-            foreach (string dll in Directory.GetFiles(name))
+            foreach (string dll in PluginPath)
             {
                 //DLLファイル以外は読まない
                 if (dll.ToLower().EndsWith(".dll") == false) continue;
+
+                //存在しないなら読み込みしない
+                if (!File.Exists(dll)) continue;
 
                 //ファイル読み込み
                 var asm = Assembly.LoadFrom(dll);
